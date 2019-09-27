6acbd9a96a7027a880a55ba9a65edab7aa0ba9bf
Jules Grand <jls.grand@gmail.com>
2019-09-27 18:04:45 +0200

    initialisation du mini projet

---
+++ nourrisson-step2.py
@@ -0,0 +1,16 @@
+# On importe les modules qui vont nous permettre de traiter les données
+
+# matplotlib pour réaliser les graphiques
+import matplotlib.pyplot as plt
+# csv pour lire les fichiers de données
+import csv
+
+
+# Récupération des mesures dans les fichiers à faire
+
+
+
+# Affichage des graphiques à faire
+
+
+
